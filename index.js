c157ba82aafb0a6a0d6c0466fe1a10aba09b1ee7
Satnam Singh <harnamsinghsandhu04@gmail.com>
2019-07-13 20:09:14 +0530

    Basic Initialization

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Noobster');
+});
+
+app.listen(3939, (err) => {
+    if (err) throw err;
+    console.log("Server Running on Port 3939");
+});
