d6d70b9711dae083fea78cc256980b1f8a44111b
Satnam Singh <harnamsinghsandhu04@gmail.com>
2019-07-13 21:19:48 +0530

    Added basic landing page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignComponent } from './sign/sign.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignComponent } from './sign/sign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,
