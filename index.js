2a0d478cc9cf0813a6b530a8d3c35c045c99a5bc
Satnam Singh <harnamsinghsandhu04@gmail.com>
2019-07-13 21:03:43 +0530

    Added DotENV

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 
